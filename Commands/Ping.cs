2df87c62a91af2facb56d2db6a444d2beb07210a
Scarlette Fox <lexidahusky.102@gmail.com>
2023-09-01 00:11:30 +0100

    rawr x3
    ...

---
+++ Commands/Ping.cs
@@ -0,0 +1,23 @@
+﻿using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+
+public class PingCommand : BaseCommandModule
+{
+    [Command("ping")]
+    public async Task Command(CommandContext ctx)
+    {
+        TimeSpan elapsedSpan = new TimeSpan(DateTime.Now.Ticks - ctx.Message.CreationTimestamp.Ticks);
+
+        DiscordEmbedBuilder embed = new DiscordEmbedBuilder
+        {
+            Color = new DiscordColor("#fe9fc6"),
+        };
+
+        embed.AddField("BOT Latency", $"```ini\n[ {ctx.Client.Ping}ms ]```", true);
+        embed.AddField("API Latency", $"```ini\n[ {elapsedSpan.Milliseconds}ms ]```", true);
+
+        await ctx.RespondAsync(embed);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.SlashCommands;
+
+namespace WorldsWorstDiscordBot
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = File.ReadAllText("config.txt").Split('\n');
+            Dictionary<string, string> openWith = new Dictionary<string, string>();
+
+            foreach (var item in config)
+            {
+                var subitems = item.Split('=');
+                openWith.Add(subitems[0], subitems[1]);
+            }
+
+            var discord = new DiscordClient(new DiscordConfiguration()
+            {
+                Token = openWith["TOKEN"],
+                TokenType = TokenType.Bot,
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+            });
+
+            var commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            {
+                StringPrefixes = new[] { ">." }
+            });
+
+            var slash = discord.UseSlashCommands();
+            slash.RegisterCommands<PingSlashCommand>();
+
+            commands.RegisterCommands<PingCommand>();
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+        }
+    }
+}
