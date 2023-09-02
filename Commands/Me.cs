d865878d19f18a395707202b74b348179d1ef701
Scarlette Fox <lexidahusky.102@gmail.com>
2023-09-02 04:30:05 +0100

    feat: added feature to track my user presence data for discord
    Why? idk, i was bored lmfaoo
    
    tysmm @TheFlyingFoool

---
+++ Commands/Me.cs
@@ -0,0 +1,58 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.SlashCommands;
+using DSharpPlus.Entities;
+using MidTierDiscordBot;
+using System.Globalization;
+
+public class TimeCommand : BaseCommandModule
+{
+    [Command("time")]
+    public async Task Command(CommandContext ctx)
+    {
+        Console.WriteLine("OwO");
+        await ctx.Message.RespondAsync(GenerateEmbed());
+    }
+
+    public static DiscordEmbedBuilder GenerateEmbed() 
+    {
+        var embed = new DiscordEmbedBuilder();
+        foreach (var activity in Program.globalActivites)
+        {
+            int.TryParse(activity["Duration"], out int activityDuration);
+
+            if (activity["Running"] == "true")
+            {
+                string dateStr = activity["StartTime"];
+                DateTime startTime = DateTime.ParseExact(dateStr, "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                TimeSpan elapsedSpan = new TimeSpan(DateTime.Now.Ticks - startTime.Ticks);
+                activityDuration += (int)elapsedSpan.TotalSeconds;
+            }
+
+            TimeSpan timeSpan = TimeSpan.FromSeconds(activityDuration);
+            string formattedTime = "";
+            if(timeSpan.Days > 0) 
+                formattedTime = string.Format("{0:%d} days, {0:%h} hours, {0:%m} minutes, {0:%s} seconds", timeSpan);
+            else if (timeSpan.Hours > 0)
+                formattedTime = string.Format("{0:%h} hours, {0:%m} minutes, {0:%s} seconds", timeSpan);
+            else if (timeSpan.Minutes > 0)
+                formattedTime = string.Format("{0:%m} minutes, {0:%s} seconds", timeSpan);
+            else
+                formattedTime = string.Format("{0:%s} seconds", timeSpan);
+
+            embed.AddField($"{activity["Type"].Replace("ListeningTo", "Listening to")} {activity["Name"]}", $" \nLength: {formattedTime}");
+        }
+
+        return embed;
+    }
+}
+
+public class TimeSlashCommand : ApplicationCommandModule
+{
+    [SlashCommand("time", "Check amount of time an application has been opened")]
+    public async Task SlashCommand(InteractionContext ctx)
+    {
+        await ctx.CreateResponseAsync(TimeCommand.GenerateEmbed());
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,17 @@
 ﻿using DSharpPlus;
 using DSharpPlus.CommandsNext;
+using DSharpPlus.Entities;
 using DSharpPlus.SlashCommands;
 using System.Reflection;
+using Newtonsoft.Json;
+using System.Globalization;
 
 namespace MidTierDiscordBot
 {
     class Program
     {
+        public static List<Dictionary<string, string>> globalActivites = new List<Dictionary<string, string>>();
+
         static async Task Main(string[] args)
         {
             string[] textdata = File.ReadAllText("config.txt").Split('\n');
@@ -15,31 +20,142 @@ namespace MidTierDiscordBot
             foreach (string item in textdata)
             {
                 string[] subitems = item.Split('=');
-                if (subitems.Length > 1) 
+                if (subitems.Length > 1)
                 {
                     config.Add(subitems[0], subitems[1]);
                 }
-               
             }
 
-            DiscordClient discord = new DiscordClient(new DiscordConfiguration()
+            DiscordClient client = new DiscordClient(new DiscordConfiguration()
             {
                 Token = config["TOKEN"],
                 TokenType = TokenType.Bot,
-                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents
+                Intents = DiscordIntents.AllUnprivileged | DiscordIntents.MessageContents | DiscordIntents.GuildPresences
             });
 
-            CommandsNextExtension commands = discord.UseCommandsNext(new CommandsNextConfiguration()
+            CommandsNextExtension commands = client.UseCommandsNext(new CommandsNextConfiguration()
             {
                 StringPrefixes = new[] { ">." }
             });
 
-            SlashCommandsExtension slash = discord.UseSlashCommands();
+            SlashCommandsExtension slash = client.UseSlashCommands();
             slash.RegisterCommands(Assembly.GetExecutingAssembly());
             commands.RegisterCommands(Assembly.GetExecutingAssembly());
 
-            await discord.ConnectAsync();
+            await client.ConnectAsync();
+            await UpdateLoop(config, client);
             await Task.Delay(-1);
+        }
+
+        // this here exists because i want to fetch my presence data every 5 seconds
+        // i want to do this so i can keep track of it in a file
+        private static async Task UpdateLoop(Dictionary<string, string> config, DiscordClient client)
+        {
+            var mainServer = await client.GetGuildAsync(ulong.Parse(config["GUILD_ID"]));
+
+            if(File.Exists("activities.json"))
+            {
+                globalActivites = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(await File.ReadAllTextAsync("activities.json"));
+            }
+            foreach (var activity in globalActivites) 
+            {
+                activity["Running"] = "false";
+                activity["EndTime"] = "";
+                activity["StartTime"] = "";
+            }
+
+            await File.WriteAllTextAsync("activities.json", JsonConvert.SerializeObject(globalActivites, Formatting.Indented));
+
+            while (true)
+            {
+                // Wait for a specified amount of time before the next update
+                if (mainServer != null && !mainServer.IsUnavailable)
+                {
+                    var userData = await mainServer.GetMemberAsync(ulong.Parse(config["USER_ID"]));
+                    if (userData != null)
+                    {
+                        if (userData.Presence != null)
+                        {
+                            bool activitesChanged = false;
+                            foreach (DiscordActivity activity in userData.Presence.Activities)
+                            {
+                                Dictionary<string, string> activityEntry = new Dictionary<string, string>();
+                                bool foundMatch = false;
+
+                                foreach (var globalActivity in Program.globalActivites)
+                                {
+                                    if (globalActivity["Name"] == activity.Name)
+                                    {
+                                        foundMatch = true;
+                                        activityEntry = globalActivity;
+                                        break;
+                                    }
+                                }
+
+                                if (!foundMatch) // This is where new entries are added
+                                {
+                                    activityEntry.Add("Name", activity.Name);
+                                    activityEntry.Add("Type", activity.ActivityType.ToString());
+                                    activityEntry.Add("StartTime", DateTime.Now.ToString());
+                                    activityEntry.Add("EndTime", "");
+                                    activityEntry.Add("Running", "true");
+                                    activityEntry.Add("Duration", "0");
+                                    globalActivites.Add(activityEntry);
+
+                                    activitesChanged = true;
+                                }
+                                else // This is where the running state is applied
+                                {
+                                    if (activityEntry["Running"] == "false")
+                                    {
+                                        activityEntry["Running"] = "true";
+                                        activityEntry["StartTime"] = DateTime.Now.ToString();
+                                        activitesChanged = true;
+                                    }
+                                }
+                            }
+
+                            foreach (var globalActivity in Program.globalActivites)
+                            {
+                                bool foundMatch = false;
+                                foreach (DiscordActivity activity in userData.Presence.Activities)
+                                {
+                                    if (globalActivity["Name"] == activity.Name)
+                                    {
+                                        foundMatch = true;
+                                        break;
+                                    }
+                                }
+
+                                if(!foundMatch)
+                                {
+                                    if (globalActivity["Running"] == "true")
+                                    {
+                                        globalActivity["Running"] = "false";
+                                        globalActivity["EndTime"] = DateTime.Now.ToString();
+                                        string dateStr = globalActivity["StartTime"];
+
+                                        DateTime startTime = DateTime.ParseExact(dateStr, "M/d/yyyy h:mm:ss tt", CultureInfo.InvariantCulture);
+
+                                        TimeSpan elapsedSpan = new TimeSpan(DateTime.Now.Ticks - startTime.Ticks);
+
+                                        int.TryParse(globalActivity["Duration"], out int activityDuration);
+
+                                        activityDuration += (int) elapsedSpan.TotalSeconds;
+
+                                        globalActivity["Duration"] = activityDuration.ToString();
+                                        activitesChanged = true;
+                                    }
+                                }
+                            }
+                       
+                            if(activitesChanged) await File.WriteAllTextAsync("activities.json", JsonConvert.SerializeObject(globalActivites, Formatting.Indented));
+                        }
+                    }
+                }
+
+                await Task.Delay(TimeSpan.FromSeconds(5));
+            }
         }
     }
 }
