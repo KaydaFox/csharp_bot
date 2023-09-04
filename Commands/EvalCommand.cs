de0dccb8ded995355be13084038620890fa7baed
TheFlyingFoool <48270355+TheFlyingFoool@users.noreply.github.com>
2023-09-04 16:42:49 -0500

    fixed eval, kayda dmed the code for that she had, idk why she didnt commit it. added ctx context weirdly

---
+++ Commands/EvalCommand.cs
@@ -0,0 +1,54 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using DSharpPlus.Entities;
+using Microsoft.CodeAnalysis.CSharp.Scripting;
+using Microsoft.CodeAnalysis.Scripting;
+
+public class EvalCommand : BaseCommandModule
+{
+    [Command("eval")]
+    public async Task Command(CommandContext ctx, [RemainingText] string codeToEval)
+    {
+        await EvaluateCode(ctx, codeToEval);
+    }
+    public class CommandContextContainer
+    {
+        public CommandContext ctx;
+    }
+    public static CommandContextContainer commandContextContainer = new CommandContextContainer();
+    public static async Task EvaluateCode(CommandContext ctx, string code)
+    {
+
+        try
+        {
+            object result = await Task.Run(async () =>
+            {
+                commandContextContainer.ctx = ctx;
+                return await CSharpScript.EvaluateAsync(code, globals: commandContextContainer);
+            });
+
+            if (result == null)
+            {
+                await ctx.RespondAsync("No result provided");
+            }
+            else
+            {
+                await ctx.RespondAsync(result.ToString());
+            }
+        }
+        catch (CompilationErrorException e)
+        {
+            await ctx.RespondAsync(e.Message);
+        }
+        
+    }
+}
+
+/*public class EvalSlashCommand : ApplicationCommandModule
+{
+    [SlashCommand("eval", "Evaluate some code")]
+    public async Task SlashCommand(InteractionContext ctx)
+    {
+        await ctx.CreateResponseAsync(EvalCommand.GenerateEmbed());
+    }
+}*/
